aaa8cf0311a042504c21f96d8228b18be5275568
Zack Urben <zackurben@gmail.com>
2016-02-05 13:08:34 -0600

    Adding better graphing capabilities, including raw access to nvd3 with a data mutator to access any graph type.

--- src/question.js
+++ src/question.js
@@ -15,7 +15,7 @@ angular.module('formio.question', ['formio', 'nvd3'])
             '{{ alert.message }}'+
           '</div>' +
           '<div class="formio-question">' +
-          formioQuestionTemplate +
+            formioQuestionTemplate +
           '</div>' +
         '</div>',
       scope: {
@@ -24,7 +24,9 @@ angular.module('formio.question', ['formio', 'nvd3'])
         submission: '=',
         previewResults: '=',
         updateAnswer: '=',
-        chartOptions: '='
+        chart: '=',
+        chartAdvanced: '=',
+        chartDataCustomizer: '='
       },
       link: function(scope, element) {
         scope.questionLoaded = false;
@@ -47,118 +49,200 @@ angular.module('formio.question', ['formio', 'nvd3'])
           $http,
           FormioUtils
         ) {
-          $scope.formio = new Formio($scope.src);
-          $scope._chartOptions = $scope.chartOptions || {
-            chart: {
-              type: 'pieChart',
-              height: 500,
-              x: function(d){return d.key;},
-              y: function(d){return d.y;},
-              showLabels: true,
-              duration: 500,
-              labelThreshold: 0.01,
-              labelSunbeamLayout: true,
-              legend: {
-                margin: {
-                  top: 5,
-                  right: 35,
-                  bottom: 5,
-                  left: 0
-                }
-              }
+          $scope.page = {};
+          $scope.form = {};
+
+          // The available graph types.
+          var types = ['table', 'pie'];
+
+          /**
+           * Generates the html for a table display of q/a.
+           */
+          var makeTable = function() {
+            return '<div class="row">' +
+              '<div class="col-md-12">' +
+                '<h3>Results</h3>' +
+                '<table class="table table-hover table-condensed">' +
+                  '<thead>' +
+                    '<tr>' +
+                      '<th>Answer</th>' +
+                      '<th>Count</th>' +
+                    '</tr>' +
+                  '</thead>' +
+                  '<tbody>' +
+                    '<tr ng-repeat="answer in data track by $index">' +
+                      '<td>{{answer.label}}</td>' +
+                      '<td>{{answer.value}}</td>' +
+                    '</tr>' +
+                  '</tbody>' +
+                '</table>' +
+              '</div>' +
+            '</div>';
+          };
+
+          /**
+           * Takes the common q/a result format and mutates it for graphing use in d3.
+           *
+           * @returns {*}
+           */
+          var normalizeData = function() {
+            if (['table'].indexOf($scope.type) !== -1) {
+              return _($scope.data)
+                .map(function(value, key) {
+                  return {
+                    label: key,
+                    value: value
+                  };
+                })
+                .value();
+            }
+            else if (['pie'].indexOf($scope.type) !== -1) {
+              return _($scope.data)
+                .map(function(value, key) {
+                  return {
+                    key: key,
+                    y: value
+                  };
+                })
+                .value();
             }
           };
 
-          $scope.page = {};
-          $scope.form = {};
-          $http.get($scope.src)
-            .then(function(data) {
-              data = data.data;
-              $scope.form = data;
+          /**
+           * Makes a graph using the current type and data.
+           *
+           * @returns {string}
+           */
+          var makeGraph = function() {
+            // Check for advanced usage.
+            if ($scope.chartAdvanced &&_.get($scope.chartAdvanced, 'chart.type') && $scope.chartDataCustomizer) {
+              // Make a nvd3 chart with the advanced options and mutate the data with the customizer.
+              $scope._data = $scope.chartDataCustomizer($scope.data);
+              return '<nvd3 options="chartAdvanced" data="_data"></nvd3>'
+            }
 
-              /**
-               * Show the analytics for this specific question.
-               */
-              $scope.showAnalytics = function() {
-                $http.get($scope.src + '/submission?limit=4294967295')
-                  .then(function(data) {
-                    data = data.data;
+            // Continue with regular usage.
+            $scope.type = $scope.type.toString().toLowerCase();
+            $scope.data = normalizeData();
 
-                    // Create a map of the responses, where key:response and value:quantity.
-                    var count = {};
-                    _(data)
-                      .map('data.' + $scope.question)
-                      .value()
-                      .forEach(function(e) {
-                        count[e] = count[e]+1 || 1;
-                      });
+            if (types.indexOf($scope.type) === -1) {
+              console.error('Unknown type given: ' + $scope.type);
+            }
 
-                    // Format the response data for the given chart type.
-                    $scope.chartData = _(count)
-                      .map(function(value, key) {
-                        return {
-                          key: key,
-                          y: value
-                        };
-                      })
-                      .value();
-
-                    $scope.questionElement.html($compile(
-                      '<span>Selection:{{results}}</span><br>' +
-                      '<nvd3 options="_chartOptions" data="chartData"></nvd3>' +
-                      '<button ng-if="updateAnswer" class="btn btn-success btn-sm" ng-click="resetQuestion()">Question</button>'
-                    )($scope));
-                  })
-                  .catch(function(err) {
-                    console.error(err);
-                  });
-              };
-
-              $scope.showQuestion = function() {
-                if (!$scope.updateAnswer) {
-                  $scope.showAnalytics();
+            if ($scope.type === 'table') {
+              return makeTable();
+            }
+            else if ($scope.type === 'pie') {
+              $scope.options = $scope.chartAdvanced || {
+                chart: {
+                  type: 'pieChart',
+                  height: 500,
+                  x: function(d){return d.key;},
+                  y: function(d){return d.y;},
+                  showLabels: true,
+                  duration: 500,
+                  labelThreshold: 0.01,
+                  labelSunbeamLayout: true,
+                  legend: {
+                    margin: {
+                      top: 5,
+                      right: 35,
+                      bottom: 5,
+                      left: 0
+                    }
+                  }
                 }
-
-                $scope.page.components = [FormioUtils.getComponent(data.components, $scope.question)];
-                var pageElement = angular.element(document.createElement('formio'));
-
-                angular.element(document.getElementsByClassName('formio-question-form'))
-                  .html($compile(pageElement.attr({
-                    form: 'page',
-                    submission: 'submission'
-                  }))($scope));
-                $scope.questionLoaded = true;
-                $scope.formioAlerts = [];
-              };
-              $scope.showQuestion();
-
-              $scope.resetQuestion = function() {
-                $scope.questionElement.html($compile(formioQuestionTemplate)($scope));
-                $scope.showQuestion();
               };
 
-              /**
-               * Save the current submission, using the form src.
-               */
-              $scope.save = function() {
-                var method = _.has($scope.submission, '_id')
-                  ? 'put'
-                  : 'post';
-                var url = (method === 'post')
-                  ? $scope.src + '/submission'
-                  : $scope.src + '/submission/' + $scope.submission._id;
-                $http[method](url, $scope.submission)
-                  .then(function() {
-                    $scope.showAnalytics();
-                  })
-                  .catch(function(err) {
-                    console.error(err);
+              return '<nvd3 options="options" data="data"></nvd3>'
+            }
+          };
+
+          $http.get($scope.src).then(function(data) {
+            data = data.data;
+            $scope.form = data;
+
+            /**
+             * Show the analytics for this specific question.
+             */
+            $scope.showAnalytics = function() {
+              $http.get($scope.src + '/submission?limit=4294967295').then(function(data) {
+                data = data.data;
+
+                // Create a map of the responses, where key:response and value:quantity.
+                $scope.data = {};
+                _(data)
+                  .map('data.' + $scope.question)
+                  .value()
+                  .forEach(function(e) {
+                    $scope.data[e] = $scope.data[e]+1 || 1;
                   });
-              };
-            })
-            .catch(function(err) {
-              console.error(err);
-            });
+
+                // Calculate the chart type and normalize the data.
+                $scope.type = $scope.chart || 'table';
+
+                // Build the answer display.
+                $scope.questionElement.html($compile(
+                  '<h3 class="fio-question-output">Answered: <span class="fio-question-answer">{{submission.data[question] || "N/A"}}</span></h3><br>' +
+                  makeGraph() +
+                  '<button ng-if="updateAnswer" class="btn btn-success btn-sm" ng-click="resetQuestion()">Question</button>'
+                )($scope));
+              })
+              .catch(function(err) {
+                console.error(err);
+              });
+            };
+
+            /**
+             * Show the currently configured question, in the formio-question-form element.
+             */
+            $scope.showQuestion = function() {
+              if (!$scope.updateAnswer) {
+                $scope.showAnalytics();
+              }
+
+              $scope.page.components = [FormioUtils.getComponent(data.components, $scope.question)];
+              var pageElement = angular.element(document.createElement('formio'));
+
+              angular.element(document.getElementsByClassName('formio-question-form'))
+                .html($compile(pageElement.attr({
+                  form: 'page',
+                  submission: 'submission'
+                }))($scope));
+              $scope.questionLoaded = true;
+              $scope.formioAlerts = [];
+            };
+            $scope.showQuestion();
+
+            /**
+             * Reset the current question view, and return the questionElement contents to the pristine state.
+             */
+            $scope.resetQuestion = function() {
+              $scope.questionElement.html($compile(formioQuestionTemplate)($scope));
+              $scope.showQuestion();
+            };
+
+            /**
+             * Save the current submission, using the form src.
+             */
+            $scope.save = function() {
+              var method = _.has($scope.submission, '_id')
+                ? 'put'
+                : 'post';
+              var url = (method === 'post')
+                ? $scope.src + '/submission'
+                : $scope.src + '/submission/' + $scope.submission._id;
+              $http[method](url, $scope.submission).then(function() {
+                $scope.showAnalytics();
+              })
+              .catch(function(err) {
+                console.error(err);
+              });
+            };
+          })
+          .catch(function(err) {
+            console.error(err);
+          });
         }
       ]
     };
